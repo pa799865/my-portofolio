a426aaeffe571bca62e12c933a818fb6c4a4f316
Apocalpsy. <pa799865@gmail.com>
2025-01-27 15:33:35 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // typing text hero
 const typed = new Typed(".typing-text", {
-  strings: ["Pelajar", "Designer", "Editor"],
+  strings: ["Pelajar", "Back-End Developer", "Editor"],
   loop: true,
   typeSpeed: 55,
   backSpeed: 25,
